b528151ccf14040deda5aab1bd6f68fb21622f06
SenX_eb85@itstepru.onmicrosoft.com <SenX_eb85@itstepru.onmicrosoft.com>
2021-10-07 22:09:19 +0500

    07.10.2021

--- Arraeyes/Source.cpp
+++ Arraeyes/Source.cpp
@@ -7,8 +7,8 @@ using  namespace std;
 void FillRand(int arr[],const unsigned int n);
 void Print(int arr[], const unsigned int n);
 void ReversPrint(int arr[], const unsigned int n);
-void Sum(int arr[], const unsigned int n, int s);
-void Avg(int arr[], const unsigned int n);
+int Sum(int arr[], const unsigned int n);
+int Avg(int arr[], const unsigned int n);
 
 
 
@@ -18,12 +18,13 @@ void  main()
 	setlocale(LC_ALL, "Russian");
 	const unsigned int n = 5;
 	int arr[n];
-	int s=0;
+	
 	FillRand(arr, n);
 	Print(arr, n);
 	ReversPrint(arr, n);
-	Sum(arr, n, s);
-	Avg(arr, n);
+	cout << endl << " Ñóììà = " << Sum(arr, n);
+
+	//Avg(arr, n);
 	return;
 
 }
@@ -53,18 +54,17 @@ void ReversPrint(int arr[], const unsigned int n)
 	}
 }
 
-void Sum(int arr[], const unsigned int n, int s)
+int Sum(int arr[], const unsigned int n)
 {
-	
+	int s = 0;
 	for (int i = 0; i < n; i++)
 	{
 		s = s + arr[i];
-		
 	}
-	cout << endl << " Summa = " << s;
+	return s;
 }
 
-void Avg(int arr[], const unsigned int n)
+int Avg(int arr[], const unsigned int n)
 {
 	int s=0;
 	for (int i = 0; i < n; i++)
