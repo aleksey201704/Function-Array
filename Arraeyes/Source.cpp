33fefeed4e2dadd9f4f1f523751c903fb72e7be6
SenX_eb85@itstepru.onmicrosoft.com <SenX_eb85@itstepru.onmicrosoft.com>
2021-10-07 20:39:45 +0500

    07.10.2021

--- Arraeyes/Source.cpp
+++ Arraeyes/Source.cpp
@@ -8,10 +8,12 @@ void FillRand(int arr[],const unsigned int n);
 void Print(int arr[], const unsigned int n);
 void ReversPrint(int arr[], const unsigned int n);
 void Sum(int arr[], const unsigned int n, int s);
+void Avg(int arr[], const unsigned int n);
 
 
 
-void main()
+
+void  main()
 {
 	setlocale(LC_ALL, "Russian");
 	const unsigned int n = 5;
@@ -21,6 +23,8 @@ void main()
 	Print(arr, n);
 	ReversPrint(arr, n);
 	Sum(arr, n, s);
+	Avg(arr, n);
+	return;
 
 }
 
@@ -58,4 +62,14 @@ void Sum(int arr[], const unsigned int n, int s)
 		
 	}
 	cout << endl << " Summa = " << s;
+}
+
+void Avg(int arr[], const unsigned int n)
+{
+	int s=0;
+	for (int i = 0; i < n; i++)
+	{
+		s = s + arr[i];
+	}
+	cout << endl << "Среднне число = " << (double)s/n;
 }
